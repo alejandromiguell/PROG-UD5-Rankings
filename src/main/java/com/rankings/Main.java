d19abf75c91d6eadbe6c6e371c1892c4839356b6
Alejandro <usr4138@salesianos-lacuesta.net>
2025-06-13 13:12:29 +0100

    Mensaje principal

--- src/main/java/com/rankings/Main.java
+++ src/main/java/com/rankings/Main.java
@@ -1,12 +1,13 @@
+//Clase main que ejecutara la aplicacion
 package main.java.com.rankings;
 
 import main.java.com.rankings.view.Ventana;
 
-// Clase principal del programa
+
 public class Main {
     
     public static void main(String[] args) {
-        // Crear y mostrar la ventana principal
+      
         Ventana ventana = new Ventana();
         ventana.setVisible(true);
     }
